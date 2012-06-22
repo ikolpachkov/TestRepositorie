7d4cfb4542a07b6fcc192a2ad29b17bef0344744
xorosho0 <sevjewelry@gmail.com>
2012-06-22 23:28:57 +0300

    first commit
    add new files

---
+++ Classes/UntitledAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  UntitledAppDelegate.h
+//  Untitled
+//
+//  Created by Igor Kolpachkov on 15.04.12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UntitledAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
